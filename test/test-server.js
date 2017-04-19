a36f22f0a0c3b1e10a0b06dc03dd1487693447c0
Nick Foden <nickfoden@gmail.com>
2017-04-19 12:57:44 -0400

    added another URL

--- test/test-server.js
+++ test/test-server.js
@@ -4,7 +4,7 @@ const faker = require('faker');
 const mongoose = require('mongoose');
 const should = chai.should();
 
-const {TEST_DATABASE_URL} = require('../config');
+const {DATABASE_URL} = require('../config');
 const {BlogPost} = require('../models');
 const {closeServer, runServer, app} =  require('../server');
 
