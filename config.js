1499bc66755f85ee80266c7e94c04f2f9e7977b8
Nick Foden <nickfoden@gmail.com>
2017-04-19 23:36:12 -0400

    Changed Database URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://nick:nickPassword@ds163360.mlab.com:63360/mongoose-blog';
+                       'mongodb://localhost/mongoose-blog';
 
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
